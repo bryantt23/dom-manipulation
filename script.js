94f48dd4fe3ae23fb413a88b6711280d5b3cdbc1
bryant tunbutr <bryantt23@users.noreply.github.com>
2020-12-20 17:57:29 -0800

    Append to dom

--- script.js
+++ script.js
@@ -6,3 +6,27 @@ content.classList.add('content');
 content.textContent = 'This is the glorious text-content!';
 
 container.appendChild(content);
+
+let p = document.createElement('p');
+p.textContent = "Hey I'm red";
+p.style = 'color:red';
+container.appendChild(p);
+
+let h3 = document.createElement('h3');
+h3.textContent = 'I’m a blue h3!';
+h3.style = 'color:blue';
+container.appendChild(h3);
+
+let div = document.createElement('div');
+div.style = 'background-color:pink; border:black;';
+
+let h1 = document.createElement('h1');
+h1.textContent = 'I’m in a div';
+
+let p2 = document.createElement('p');
+p2.textContent = 'ME TOO!';
+
+div.appendChild(h1);
+div.appendChild(p2);
+
+container.appendChild(div);
